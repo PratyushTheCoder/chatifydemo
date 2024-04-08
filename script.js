81512b3f39e3b43b8b9e1b5aed89c9861faf15e8
PratyushTheCoder <pratyush.jha299gamil.com>
2024-04-08 13:22:55 +0530

    wikipidea chat added

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const chatai=document.getElementById("chat-ai");
+const chatwiki=document.getElementById("chat-wiki");
 
+
+// Chat logic for the AI chat bot
 chatai.onclick=()=>{
-    
+    console.log("the ai chat was clicked!");
 }
+
+
+// Chat logic for the wikipedia chatbot
+chatwiki.onclick=()=>{
+    console.log("The wikipedia chat was clicked");
+}
+
