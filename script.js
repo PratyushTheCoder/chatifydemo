9103f66569b555e4054b003a4560743fb6c8f503
PratyushTheCoder <pratyush.jha299gamil.com>
2024-04-08 14:33:55 +0530

    Chat window created

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const chatai=document.getElementById("chat-ai");
 const chatwiki=document.getElementById("chat-wiki");
-const chat_window=document.getElementById("");
+const chat_window=document.getElementById("message_container");
+const chat_close=document.getElementById("close_message");
+const chat_show=document.getElementById("show_chat");
+const chat_title=document.getElementById("chat_title");
 
 
 // Chat logic for the AI chat bot
 chatai.onclick=()=>{
     console.log("the ai chat was clicked!");
+    chat_title.innerText="AI Chat";
+    chat_window.style.display="block";
 }
 
 
 // Chat logic for the wikipedia chatbot
 chatwiki.onclick=()=>{
-    
+    chat_title.innerText="WikiChat";
+    chat_window.style.display="block";
 }
 
+// Chat window logic
+chat_close.onclick=()=>{
+    console.log("Close!");
+    chat_window.style.display="none";
+}
+chat_show.onclick=()=>{
+    console.log("Show");
+    chat_window.style.display="block";
+}
